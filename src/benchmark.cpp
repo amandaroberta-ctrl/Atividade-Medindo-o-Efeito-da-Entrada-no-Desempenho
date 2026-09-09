1a9f366882fe84b80403a4368aeeb70ffa3bfacf
amandaroberta-ctrl <amandaroberta@rede.ulbra.br>
2026-09-09 14:27:34 -0300

    Testando com dados aleatórios e ordenados e medição de tempo

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -28,7 +28,6 @@ vector<int> criarDadosAleatorios(int tamanho, mt19937& gerador) {
 void insertionSort(vector<int>& dados) {
 
     for (int i = 1; i < dados.size(); i++) {
-
         int chave = dados[i];
         int j = i - 1;
 
@@ -59,7 +58,6 @@ void merge(vector<int>& dados, int inicio, int meio, int fim) {
     int k = inicio;
 
     while (i < esquerda.size() && j < direita.size()) {
-
         if (esquerda[i] <= direita[j]) {
             dados[k] = esquerda[i];
             i++;
@@ -67,7 +65,6 @@ void merge(vector<int>& dados, int inicio, int meio, int fim) {
             dados[k] = direita[j];
             j++;
         }
-
         k++;
     }
 
@@ -94,46 +91,52 @@ void mergeSort(vector<int>& dados, int inicio, int fim) {
 
     mergeSort(dados, inicio, meio);
     mergeSort(dados, meio + 1, fim);
-
     merge(dados, inicio, meio, fim);
 }
 
 long long medirTempoInsertion(vector<int> dados) {
-
     auto inicio = chrono::high_resolution_clock::now();
-
     insertionSort(dados);
-
     auto fim = chrono::high_resolution_clock::now();
-
     return chrono::duration_cast<chrono::microseconds>(fim - inicio).count();
 }
 
 long long medirTempoMerge(vector<int> dados) {
-
     auto inicio = chrono::high_resolution_clock::now();
-
     mergeSort(dados, 0, dados.size() - 1);
-
     auto fim = chrono::high_resolution_clock::now();
-
     return chrono::duration_cast<chrono::microseconds>(fim - inicio).count();
 }
 
 int main() {
-
     mt19937 gerador(2026);
-
     for (int tamanho : tamanhos) {
-
         vector<int> dadosAleatorios =
             criarDadosAleatorios(tamanho, gerador);
-
         vector<int> dadosOrdenados = dadosAleatorios;
-
         sort(dadosOrdenados.begin(), dadosOrdenados.end());
 
         cout << "Tamanho: " << tamanho << endl;
+        long long tempoInsertion = medirTempoInsertion(dadosAleatorios);
+
+        cout << "Insertion Sort (aleatorio): "
+            << tempoInsertion << " microssegundos" << endl;
+     
+        long long tempoInsertionOrdenado = medirTempoInsertion(dadosOrdenados);
+
+        cout << "Insertion Sort (ordenado): "
+            << tempoInsertionOrdenado << " microssegundos" << endl;
+     
+        long long tempoMerge = medirTempoMerge(dadosAleatorios);
+
+        cout << "Merge Sort (aleatorio): "
+            << tempoMerge << " microssegundos" << endl;
+            
+        long long tempoMergeOrdenado = medirTempoMerge(dadosOrdenados);
+
+        cout << "Merge Sort (ordenado): "
+            << tempoMergeOrdenado << " microssegundos" << endl;
+
     }
 
     return 0;
